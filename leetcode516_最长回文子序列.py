36035da335f52be06052a8e58cb39e6510e3aeb6
gongtian1234 <46668010+gongtian1234@users.noreply.github.com>
2021-08-28 14:16:30 +0000

    update 最长回文子序列（可以不连续）与最长回文子串（回文子串必须是连续的）的对比
    leetcode516与leetcode5的对比，但是都可以使用动态规划创建一个状态转移矩阵的方法解决

--- leetcode516_最长回文子序列.py
+++ leetcode516_最长回文子序列.py
@@ -19,3 +19,31 @@ def longestPalindromeSubseq(s):
                 rst[i][j] = max(rst[i+1][j], rst[i][j-1])
     return rst[0][m-1]
         
+def solution(s):
+    # leetcode5:最长回文子串
+    m = len(s)
+    if m<1:
+        return 0
+    elif m==1:
+        return 1
+    arr = [[0]*m for _ in range(m)]
+    tmp_rst = ''  # 存储最长回文子串
+    for i in range(m):
+        arr[i][i] = 1
+    max_length = 1
+    for i in range(m-1):
+        if s[i]==s[i+1]:
+            arr[i][i+1] = 1
+            max_length = 2
+            tmp_rst = s[i:i+2]
+    for length in range(2,m,1):  # length表示的是首尾字符的间隔长度
+        for i in range(m-length):
+            j = i + length
+            if arr[i+1][j-1]==1 and s[i]==s[j]:
+                arr[i][j] = 1
+                max_length = length + 1
+                tmp_rst = s[i:j+1]
+    # for i in range(m):
+    #     print(arr[i])
+    print(tmp_rst)
+    return max_length
